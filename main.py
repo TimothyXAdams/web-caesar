51b61e510cc3fe388492814c12b6c4451f1b92ac
Timothy Adams <TimothyXAdams@gmail.com>
2017-06-22 18:59:44 -0700

    added ID to textarea tag

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ form = """
                   Rotate by:
                   <input type="text" id="rotate-factor" name="rot" value='0'/>
               </label>
-              <textarea name="text">{0}</textarea>
+              <textarea id="text" name="text">{0}</textarea>
               <input type="submit" value="Submit Query"/>
           </form>
     </body>
